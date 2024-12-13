97b7d5f86a2359ed4f3d145a5383eea29e35bcf2
lcalorg3006@g.educaand.es <lcalorg3006@g.educaand.es>
2024-12-13 10:09:43 +0100

    pista

--- lib/screens/pista_screen_col.dart
+++ lib/screens/pista_screen_col.dart
@@ -1,19 +1,15 @@
 import 'package:examen_col/widgets/custom_card_tipo_2.dart';
+import 'package:examen_col/theme/app_theme.dart';
+
 import 'package:flutter/material.dart';
 
-class PistaScreen extends StatelessWidget {
-     final String characterName;
-  final String imageUrl;
-  final String description;
-   
-  const PistaScreen({Key? key, required this.characterName, required this.imageUrl, required this.description}) : super(key: key);
-  
-
+class CardScreen extends StatelessWidget {
+  const CardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
+          appBar: AppBar(
       
       title: const Text("Flutter App"),
       actions: const [
@@ -28,27 +24,17 @@ class PistaScreen extends StatelessWidget {
       //backgroundColor: Colors.blueAccent,
       elevation: 0,
      ),
-      body: Column(
-        children: [
-          CustomCardTipo2(
-            imageUrl: imageUrl,
-            nombre: characterName,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              description,
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
-        floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.close),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      ),
-    );
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: const [
+            
+            SizedBox(height: 10),
+            CustomCardTipo2(nombre: 'Pista de padel',imageUrl: 'https://urbancolex.com/wp-content/uploads/2022/10/pista-padel-cristal-300x300.png',),
+              SizedBox(height: 10),
+            CustomCardTipo2(nombre: 'Piscina Cubierta',imageUrl:  'https://portalpro.es/app/uploads/2024/01/piscina-cubierta-en-casa-1200x450.jpg'),
+             SizedBox(height: 10),
+          
+          ],
+        ));
   }
 }
